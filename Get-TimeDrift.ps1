5773830e454a2f90a02865e9d8150a34d232d57a
hydramail <andrew@hydramail.net>
2016-10-14 12:36:24 +0100

    Update Get-TimeDrift.ps1

--- Get-TimeDrift.ps1
+++ Get-TimeDrift.ps1
@@ -10,7 +10,6 @@
 #   based on their drift relative to an external source
 
 #   WinRM needs to be enabled on the remote servers
-#   Servers need to be on Windows 2012 due to the Invoke-WebRequest command
 
 
 #empty array of servers
